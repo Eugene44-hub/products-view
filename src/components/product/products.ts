6eb4401e613f65a8641def4496019ae3f302a1af
Eugenics <eugeneishado44@gmail.com>
2024-05-29 20:50:04 +0100

    changing product id

--- src/components/product/products.ts
+++ src/components/product/products.ts
@@ -28,7 +28,7 @@ export const products = [
       "PlayStation 3, video game console released in 1994 by Sony Computer Entertainment. The PlayStation, one of a new generation of 32-bit consoles, signaled Sony’s rise to power in the video game world. Also known as the PS One, the PlayStation used compact discs (CDs), heralding the video game industry’s move away from cartridges. <br/> <br/> After a failed venture with Nintendo to release the PlayStation as the Super Nintendo Entertainment System–CD in the early 1990s, Sony made the decision to market its own console. The PlayStation was released in Japan in December 1994, and it made its American debut in September 1995; both releases elicited critical acclaim and impressive sales. Titles such as Twisted Metal and Ridge Racer were very popular. By 2005 the PlayStation had become the first console ever to ship 100 million units. PlayStation’s premier games included fan favourites such as Final Fantasy 7, Crash Bandicoot, and Tekken, all of which spawned numerous sequels. <br/> <br/> Computer chip. computer. Hand holding computer chip. Central processing unit (CPU). history and society, science and technology, microchip, microprocessor motherboard computer Circuit Board <br/> <br/>The original PlayStation continued production until March 2005, when it was eclipsed by the next generation of 64-bit consoles, including Sony’s own PlayStation 2 (PS2; introduced in the early 2000s).",
   },
   {
-    id: 2,
+    id: 4,
     title: "Playstation 4",
     amount: 120000,
     image: playstationFour,
